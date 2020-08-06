5bf994326a7ea90c5e386000a268e5515d9cae22
Katie Le <lethikatie@gmail.com>
2020-08-06 23:18:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,15 @@
 class Song 
-  attr_accessor 
+  @@all = []
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
 end 
