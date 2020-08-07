b719b9a0700b0cb4766ff9672f9f0c2e4127494d
Katie Le <lethikatie@gmail.com>
2020-08-07 00:29:57 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,6 +3,7 @@ class Appointment
   attr_accessor :date, :patient, :doctor
   def initialize(date, patient, doctor)
     @date = date
+    @patient = patient
     @doctor = doctor
     @@all << self 
   end 
